d6a0a55a49e88727ced031da3eab3a3f9a680caa
Saloni Chauhan <schauhan1@hawk.iit.edu>
2023-07-29 00:04:02 -0500

    added main inde code and also added private key and put request

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 
@@ -8,7 +9,20 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async(req, res) => {
     const { username } = req.body;
-    return res.json({ username: username, secret: "sha256..."});
+
+    try {
+        const r = await axios.put(
+            'https://api.chatengine.io/users/',
+            { username: username, secret: username, first_name: username },
+            { headers: { "private-key": "89592ac1-5951-4c87-9368-53253baab107" } }
+        );
+        return res.status(r.status).json(r.data);
+    } catch(e) {
+        return res.status(e.response.status).json(e.response.data0);
+    }
 });
 
 app.listen(3001);
+
+//46951dbc-fb52-44bf-a76c-a34ab2545e77 - project id
+//89592ac1-5951-4c87-9368-53253baab107 - private key
